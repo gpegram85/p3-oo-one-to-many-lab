e05f86f7b8c4a3f3a92339782a0c8ef5335e418d
Grey Pegram <dpegram85@gmail.com>
2024-11-12 20:10:32 -0500

    Completed Labs

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,31 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    
+    all = []
+
+    def __init__(self, name, pet_type, owner=""):
+        
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise TypeError("pet_type is not valid.")
+         
+        self.name = name
+        self.owner = owner
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("This is not a pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        sorted_pets = sorted(self.pets(), key=lambda pet: pet.name)
+        return sorted_pets
